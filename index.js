f328d67bfc89868c5547239421ca51bf9f6851f9
Edwin martinez <42719323+MataXD@users.noreply.github.com>
2025-06-25 18:07:38 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import productosRouter from './routes/productos.js';
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/productos', productosRouter);
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
